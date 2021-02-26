1da418acc224923fa5428e9682ff20be77de1536
Titania Emaniar <titaniaemaniar19@gmail.com>
2021-02-26 11:29:09 +0700

    done

---
+++ aktivitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN 
+print("percabangan pada bahasa pemrograman Python")
+print("pilih bahasa Pemrograman: ")
+print("1.Phython")
+print("2. C")
+
+pilih=int(input("pilih menu(1/2):"))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("bahasa pemrograman yang dipilih adalah Python")
+else :
+    print ("pilihan Anda tidak tersedia!!!")
---
+++ aktivitas5.py
@@ -0,0 +1,7 @@
+x = int(input("masukkan tinggi: "))
+y = 1
+
+while(y<=x):
+    print("*" * (y))
+    y=y+1
+
