ec08b1f6eaa466302945311ad7aad76e58a9cdb4
Titania Emaniar <titaniaemaniar19@gmail.com>
2021-03-05 16:40:58 +0700

    done

--- Tugas1.py
+++ Tugas1.py
@@ -1,12 +1,14 @@
 
 class student:
     """ A class representing a student"""
-    def __init__ (self,n,a,g,s,t):
+    def __init__ (self,n,a,g,s,u,t):
         self.full_name=n
         self.age=a
         self.gender=g
         self.status=s
+        self.universitas=u
         self.alamat=t
+       
      
    
     def get_age(self):
@@ -15,12 +17,15 @@ class student:
         return self.gender
     def get_status(self):
         return self.status
+    def get_universitas(self):
+        return self.universitas
     def get_alamat(self):
         return self.alamat
      
-f = student ("titania emaniar", 20, "Female", "Mahasiswa", "Bogor")
-print ("Nama :", f.full_name)
-print ("Umur :", f.get_age())
-print ("gender :", f.get_gender()) 
-print ("status :", f.get_status())
-print ("alamat :", f.get_alamat())
+f = student ("Titania Emaniar", 21, "Female", "Mahasiswa","UPI", "Bogor")
+print ("Nama        :", f.full_name)
+print ("Umur        :", f.get_age())
+print ("Gender      :", f.get_gender()) 
+print ("Status      :", f.get_status())
+print ("Universitas :", f.get_universitas())
+print ("Alamat Asal :", f.get_alamat())
